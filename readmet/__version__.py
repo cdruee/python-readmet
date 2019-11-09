09058e654a47ac0c5315a64390cfac25df07633e
Clemens Druee <druee@uni-trier.de>
2019-11-09 12:13:31 +0100

    fixed version number: new release

--- readmet/__version__.py
+++ readmet/__version__.py
@@ -3,7 +3,7 @@
 __title__ = 'readmet'
 __description__ = 'Python library for reading and writing rare formats used in Meteorology.'
 __url__ = ''
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __author__ = u'Clemens Drüe'
 __author_email__ = 'druee@uni-trier.de'
 __license__ = 'All rights reserved'
