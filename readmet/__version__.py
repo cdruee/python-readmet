b7e1beade2c98f9e92be054c88f8d7e53f175c26
Clemens Druee <druee@uni-trier.de>
2022-08-07 20:44:03 +0200

    bump patch level

--- readmet/__version__.py
+++ readmet/__version__.py
@@ -3,7 +3,7 @@
 __title__ = 'readmet'
 __description__ = 'Python library for reading and writing rare formats used in Meteorology.'
 __url__ = ''
-__version__ = '0.5.0'
+__version__ = '0.5.2'
 __author__ = u'Clemens Drüe'
 __author_email__ = 'druee@uni-trier.de'
 __license__ = 'All rights reserved'
